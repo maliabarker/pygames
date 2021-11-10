6c653edad9db6ef00eedd908b48217db75ddd603
maliabarker <89502882+maliabarker@users.noreply.github.com>
2021-11-10 14:20:54 -0800

    practiced using sprites and surfaces with images

--- game.py
+++ game.py
@@ -1,68 +1,50 @@
-import pygame 
+# Import and initialize pygame
+import pygame
 pygame.init()
-
+# Configure the screen
 screen = pygame.display.set_mode([500, 500])
+# Create a new instance of Surface
+class GameObject(pygame.sprite.Sprite):
+  def __init__(self, x, y, image):
+    super(GameObject, self).__init__()
+    # self.surf = pygame.Surface((width, height))
+    # self.surf.fill((255, 0, 255))
+    self.surf = pygame.image.load(image)
+    self.x = x
+    self.y = y
 
-# Create the game loop
-running = True 
-while running: 
-    # Looks at events 
+  def render(self, screen):
+    screen.blit(self.surf, (self.x, self.y))
+
+# Make an instance of GameObject
+apple1 = GameObject(70, 70, 'imgs/apple.png')
+strawberry1 = GameObject(250, 70, 'imgs/strawberry.png')
+apple2 = GameObject(430, 70, 'imgs/apple.png')
+strawberry2 = GameObject(70, 250, 'imgs/strawberry.png')
+apple3 = GameObject(250, 250, 'imgs/apple.png')
+strawberry3 = GameObject(430, 250, 'imgs/strawberry.png')
+apple4 = GameObject(70, 430, 'imgs/apple.png')
+strawberry4 = GameObject(250, 430, 'imgs/strawberry.png')
+apple5 = GameObject(430, 430, 'imgs/apple.png')
+# Creat the game loop
+running = True
+while running:
+    # Looks at events
     for event in pygame.event.get():
-	    if event.type == pygame.QUIT:
-		    running = False
-	
-    # Clear the screen
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Clear screen
     screen.fill((255, 255, 255))
-    # Colored circles
-    '''
-    tomato = (255, 99, 71)
-    orange = (255, 160, 0)
-    yellow = (255, 225, 0)
-    green = (152, 251, 152)
-    blue = (135, 206, 250)
-
-    position1 = (70, 70)
-    position2 = (430, 70)
-    position3 = (250, 250)
-    position4 = (70, 430)
-    position5 = (430, 430)
-    pygame.draw.circle(screen, tomato, position1, 60)
-    pygame.draw.circle(screen, orange, position2, 60)
-    pygame.draw.circle(screen, yellow, position3, 60)
-    pygame.draw.circle(screen, green, position4, 60)
-    pygame.draw.circle(screen, blue, position5, 60)
-    '''
-    # Circle grid
-    x = 0
-    while x < 9:
-        #first row
-        positionx = 70
-        positiony = 70
-
-        while x <= 3:
-            pygame.draw.circle(screen, (100, 100, 100), (positionx, positiony), 60)
-            positionx += 180
-            x += 1
-        
-        positiony += 180
-        while x in range(4, 7):
-            
-            if positionx > 430:
-                positionx = 70
-            
-            pygame.draw.circle(screen, (100, 100, 100), (positionx, positiony), 60)
-            positionx += 180
-            x += 1
-        
-        positiony += 180
-        while x in range(7, 10):
-            if positionx > 430:
-                positionx = 70
-            pygame.draw.circle(screen, (100, 100, 100), (positionx, positiony), 60)
-            positionx += 180
-            x += 1
-
-        
-
-    # Update the display
+    # Draw box
+    apple1.render(screen)
+    strawberry1.render(screen)
+    apple2.render(screen)
+    strawberry2.render(screen)
+    apple3.render(screen)
+    strawberry3.render(screen)
+    apple4.render(screen)
+    strawberry4.render(screen)
+    apple5.render(screen)
+    # Update the window
     pygame.display.flip()
